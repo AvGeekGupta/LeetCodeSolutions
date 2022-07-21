4cb9c3dda451c7f36a78e8c9006fadec47409465
Utkarsh Gupta <500075374@stu.upes.ac.in>
2022-07-22 00:29:02 +0530

    Adding readme

---
+++ 14.longest-common-prefix.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=14 lang=cpp
+ *
+ * [14] Longest Common Prefix
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string ans = "";
+        char temp;
+        while
+    }
+};
+// @lc code=end
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# Leet Code Solution
+This is a repository is for solutions for [Leet Code problems](https://leetcode.com/problems/).
